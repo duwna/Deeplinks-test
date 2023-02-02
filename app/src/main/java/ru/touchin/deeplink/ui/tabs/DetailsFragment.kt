03d43f279841931d651d1342bdaedac7c25f37f6
Grigorii <leogrisha@mail.ru>
2023-02-02 14:37:50 +0400

    Add RoboSwag submodule

--- app/src/main/java/ru/touchin/deeplink/ui/tabs/DetailsFragment.kt
+++ app/src/main/java/ru/touchin/deeplink/ui/tabs/DetailsFragment.kt
@@ -1,7 +1,12 @@
 package ru.touchin.deeplink.ui.tabs
 
 import androidx.fragment.app.Fragment
+import ru.touchin.deeplink.R
+import ru.touchin.deeplink.databinding.FragmentDetailsBinding
+import ru.touchin.roboswag.navigation_base.fragments.viewBinding
 
-class DetailsFragment : Fragment() {
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+
+    private val binding by viewBinding(FragmentDetailsBinding::bind)
 
 }
